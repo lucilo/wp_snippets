ef224651d8b12e4bffb3ad1d712343ea85da06d1
appus50 <gui.chauveau@gmail.com>
2012-10-09 14:25:27 +0200

    Adding a few snippets

---
+++ plugins_snippets.php
@@ -0,0 +1,14 @@
+<?php
+
+// Désactivation Plugin updates
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+
+// Désactiver la notification de MAJ
+if ( !current_user_can('administrator') ) {
+    add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
+    add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
+}
+
+// Désactivation admin bar
+show_admin_bar(false); // dans functions.php
