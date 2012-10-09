afef14e3c2383c35195ba399459227cebcb25fab
lucilo <familylecomte@gmail.com>
2012-10-09 18:48:18 +0300

    Update plugins_snippets.php
    rajout du code pour custom post

--- plugins_snippets.php
+++ plugins_snippets.php
@@ -1,14 +1,26 @@
 <?php
 
-// Désactivation Plugin updates
+// Dï¿½sactivation Plugin updates
 remove_action( 'load-update-core.php', 'wp_update_plugins' );
 add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
 
-// Désactiver la notification de MAJ
+// Dï¿½sactiver la notification de MAJ
 if ( !current_user_can('administrator') ) {
     add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
     add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
 }
 
-// Désactivation admin bar
+// Dï¿½sactivation admin bar
 show_admin_bar(false); // dans functions.php
+
+// code PHP pour rajouter des customs post type dans l'admin de wordpress
+register_post_type('post_type_name', array(
+    'label' => __('Label du post type au puriel'),
+    'singular_label' => __('label du post type au singulier'),
+    'public' => true,
+    'exclude_from_search' => false,
+    'show_ui' => true,
+    'capability_type' => 'post',
+    'hierarchical' => false,
+    'supports' => array('title', 'author', 'excerpt', 'editor', 'thumbnail', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes')
+));
